20dc316dcbb279c8452357846871fb372dfc0547
Wenderson Farias <wenderson.farias10@gmail.com>
2024-05-03 22:52:24 +0000

    Adicionado pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home(){
+    const content = (<div>
+                        <h1>Anel</h1>
+                        <img src="https://img.irroba.com.br/fit-in/1100x900/filters:fill(fff):quality(90)/fabricad/catalog/produtos/aneis/100502/aneis-solitariobrilhante-2.jpg"/>
+                    </div>)
+    return content;
+}
+
+export default Home;
