b5862561ad8075ca7b48660025339d48083074ca
Wenderson Farias <wenderson.farias10@gmail.com>
2024-05-04 20:50:51 -0300

    Chamando imagem no front

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home(){
     const content = (<div>
                         <h1>Anel</h1>
-                        <img src="https://img.irroba.com.br/fit-in/1100x900/filters:fill(fff):quality(90)/fabricad/catalog/produtos/aneis/100502/aneis-solitariobrilhante-2.jpg"/>
+                        <img src="../images/denso-malisa.jpg"/>
                     </div>)
     return content;
 }
