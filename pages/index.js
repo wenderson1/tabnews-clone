d54e30faf7a96539e24ebb711594e3aeb642574a
Wenderson Farias <wenderson.farias10@gmail.com>
2024-05-04 20:54:52 -0300

    ajustado erro de sintaxe

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     const content = (<div>
-                        <h1>Não fica brava se eu não consegui te atender amorzinho! :< </h1>
+                        <h1>Não fica brava se eu não consegui te atender amorzinho! 🙁 </h1>
                         <img src="https://github.com/wenderson1/tabnews-clone/blob/main/images/denso-malisa.jpg"/>
                     </div>)
     return content;
